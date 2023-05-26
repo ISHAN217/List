213bc3717c721b5b94323b27a52fb20d1f3328ac
Ishan Bhardwaj <100227327+ISHAN217@users.noreply.github.com>
2023-05-26 07:41:14 +0530

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -1,34 +1,17 @@
 import streamlit as st
 from PIL import Image
-
-import torchvision.transforms as transforms
-
-class ImageCaptioningModel:
-    def __init__(self, cnn_model, encoder, decoder):
-        self.cnn_model = cnn_model
-        self.encoder = encoder
-        self.decoder = decoder
-
-    def generate_captions(self, image):
-        # Implement the caption generation logic using the model
-        # Return the generated captions
-
-        captions = ["Caption 1", "Caption 2", "Caption 3"]
-        return captions
+from model import get_caption_model, generate_caption
 
 def main():
     st.title("Image Captioning Web App")
     st.write("Upload an image and get AI-generated captions!")
 
     # Load pre-trained model
-    model_weights_path = "/content/image_captioning_transformer_weights.h5" # Specify the path to your pre-trained model weights
+    model_weights_path = "C:/Users/KIIT/Downloads/Listed/model.h5"  # Specify the path to your pre-trained model weights
+    model = get_caption_model()
 
-    # Define the image transformation
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    # Load the model weights
+    model.load_weights(model_weights_path)
 
     # File uploader
     uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
@@ -39,24 +22,12 @@ def main():
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
         # Process the image and generate captions
-        captions = generate_captions(image, model_weights_path, transform)
+        captions = generate_caption(image, model)
 
         # Display the captions
         st.header("Generated Captions")
         for caption in captions:
             st.write("- " + caption)
-
-def generate_captions(image, model_weights_path, transform):
-    # Load pre-trained model
-    model = ImageCaptioningModel(None, None, None)  # Initialize with None for encoder, decoder, and cnn_model
-   
-
-    # Pre-process the image
-    image = transform(image).unsqueeze(0)
-
-    # Perform inference
-    captions = model.generate_captions(image)
-    return captions
 
 if __name__ == '__main__':
     main()
